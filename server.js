db4d186f122f15c391e9743e31a3409a97932c8d
yunji0387 <yunji0387@gmail.com>
2023-12-18 02:10:12 -0600

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //jshint esversion:6
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
 require("dotenv").config();
